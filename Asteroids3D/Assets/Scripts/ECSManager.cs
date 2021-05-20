2cea75447f2f71446ce96fee48b4be06ef29068b
persisduaik <persisduaik@gmail.com>
2021-05-20 09:51:43 +0100

    Adding spawning Asteroids logic.

--- Asteroids3D/Assets/Scripts/ECSManager.cs
+++ Asteroids3D/Assets/Scripts/ECSManager.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 
 public class ECSManager : MonoBehaviour
 {
+    // Prefabs to convert into Entities.
+    public GameObject playerPrefab;
+    public GameObject missilePrefab;
+    public GameObject asteroidPrefab;
+
+    public int numberOfAsteroids = 1000;
+    public int areaSize = 512;
+
     private void Start()
     {
         // Initialize manage using world default.
@@ -12,13 +20,26 @@ public class ECSManager : MonoBehaviour
         var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
 
         // Convert prefabs into entity.
-        var playerEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(GameDataManager.singleton.playerPrefab, settings);
-        var missileEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(GameDataManager.singleton.playerPrefab, settings);
+        var playerEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(playerPrefab, settings);
+        var missileEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(missilePrefab, settings);
+        var asteroidEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(asteroidPrefab, settings);
 
         // Instantiate player.
-        Entity instance = manager.Instantiate(playerEntity);
-        manager.SetComponentData(instance, new Translation { Value = float3.zero });
-        manager.SetComponentData(instance, new Rotation { Value = quaternion.identity });
-        manager.SetComponentData(instance, new PlayerData { Missile = missileEntity });
+        Entity playerInstance = manager.Instantiate(playerEntity);
+        manager.SetComponentData(playerInstance, new Translation { Value = float3.zero });
+        manager.SetComponentData(playerInstance, new Rotation { Value = quaternion.identity });
+        manager.SetComponentData(playerInstance, new PlayerData { Missile = missileEntity });
+
+        // Populate the world with Asteroids.
+        for (int i = 0; i < areaSize; i++)
+        {
+            Entity asteroidInstance = manager.Instantiate(asteroidEntity);
+            float3 randomPosition = new float3(UnityEngine.Random.Range(-areaSize, areaSize),
+                                               UnityEngine.Random.Range(-areaSize, areaSize),
+                                               UnityEngine.Random.Range(-areaSize, areaSize)
+                                               );
+            manager.SetComponentData(asteroidInstance, new Translation { Value = randomPosition });
+            manager.SetComponentData(asteroidInstance, new Rotation { Value = quaternion.identity });
+        }
     }
 }
