779e74b2150814a329246a0d1999b92fd1842ce1
persisduaik <persisduaik@gmail.com>
2021-05-20 09:52:39 +0100

    Cleaning Data Manager for now and keeping the code as simple as possible.

--- Asteroids3D/Assets/Scripts/GameDataManager.cs
+++ Asteroids3D/Assets/Scripts/GameDataManager.cs
@@ -1,17 +1,9 @@
 using UnityEngine;
-using Unity.Mathematics;
 
 public class GameDataManager : MonoBehaviour
 {
     // Allow DOTS to access the Data Manager.
     public static GameDataManager singleton;
-
-    // Player prefab.
-    public GameObject playerPrefab;
-    public GameObject missilePrefab;
-
-    // Player output
-    public quaternion playerRotation { get; set; }
 
     private void Awake()
     {
@@ -24,12 +16,5 @@ public class GameDataManager : MonoBehaviour
         {
             singleton = this;
         }
-
-        playerRotation = quaternion.identity;
-    }
-
-    private void Update()
-    {
-        print(playerRotation);
     }
 }
