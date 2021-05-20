c3d9b7ecff30aeb7e534eefd87a5307aa3f87d3d
persisduaik <persisduaik@gmail.com>
2021-05-20 16:17:54 +0100

    Camera rotation improvements.

--- Asteroids3D/Assets/Scripts/FollowEntity.cs
+++ Asteroids3D/Assets/Scripts/FollowEntity.cs
@@ -27,8 +27,9 @@ public class FollowEntity : MonoBehaviour
         Translation entPos = manager.GetComponentData<Translation>(entityToFollow);
         Rotation entRot = manager.GetComponentData<Rotation>(entityToFollow);
         transform.rotation = entRot.Value;
+        transform.Rotate(new Vector3(10, 0, 0));
         float3 forward = math.mul(entRot.Value, new float3(0, 0, 1));
         float3 up = math.mul(entRot.Value, new float3(0, 1, 0));
-        transform.position = entPos.Value - forward * 10 + up * 2;
+        transform.position = entPos.Value - forward * 40 + up * 15;
     }
 }
